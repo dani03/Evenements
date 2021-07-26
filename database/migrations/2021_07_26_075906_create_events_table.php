1aebb8925e6bb73204444959858a143cc0c9260a
daniel <lesamessipa@gmail.com>
2021-07-26 14:55:17 +0200

    mise en place des models

---
+++ database/migrations/2021_07_26_075906_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('description');
+            $table->timestamp('start_at');
+            $table->timestamp('end_at');
+            $table->foreignIdFor(User::class);
+
+            $table->bolean('premium');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
